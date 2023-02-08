0a4e9f61c312f4e7a5dcb5587e6ade67219dfd55
Jose Reyes <jo.reyesgon@gmail.com>
2023-02-08 18:31:22 -0300

    minor changes

--- src/js/components/SimpleCounter.js
+++ src/js/components/SimpleCounter.js
@@ -4,17 +4,16 @@ const SimpleCounter = () => {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="container my-5">
-            <div className="card border border-primary text-center my-5">
+        <div className="container">
+            <div className="card border border-secondary text-center my-5">
                 <div className="card-body">
-                    <h1 className="text-muted">Simple Counter</h1>
-                    <div className="my-5">
-                        <h2 className="my-5 text-primary">{count}</h2>
-                        <button className="btn btn-success mx-3" onClick={() => setCount(count + 1)}>+</button>
-                        <button className="btn btn-danger mx-3"onClick={() => setCount(count - 1)} disabled={count ===0}>-</button>
-                        <button className="btn btn-warning mx-3 text-light"onClick={() => setCount(0)} disabled={count ===0}>Resert</button>
-
-                    </div>
+                    <h1 className="header my-2"><u>Simple Counter</u></h1>
+                    
+                        <h2 className=" text-primary my-4">{count}</h2>
+                        <button className="btn btn-success mx-3 my-3" onClick={() => setCount(count + 1)}>+</button>
+                        <button className="btn btn-danger mx-3" onClick={() => setCount(count - 1)} disabled={count === 0}>-</button>
+                        <button className="btn btn-warning mx-3 text-dark" onClick={() => setCount(0)} disabled={count === 0}>Resert</button>
+                    
                 </div>
             </div>
         </div>
